5b05452b178b64ca031fa77ce5b04bc86e72a8f9
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2022-12-09 10:12:03 +0000

    fix printing

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -17,4 +17,7 @@
       <tr
         v-for="a in assets"
         :key="a.id"
-        class="border-b border-gray-200 hover:bg-gray-100 hover:bg-ora
+        class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
+      >
+        <td>
+          <i
