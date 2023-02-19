c49c5a75f6ed8a632393b9efb31bdb9323d32cb8
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2023-02-19 21:13:25 +0000

    Spell

--- src/components/PxButton.vue
+++ src/components/PxButton.vue
@@ -1,4 +1,4 @@
 <template>
   <button
     @click="buttonClick"
-    class="bg-transparent hover:bg-green-500 tex
+    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-
