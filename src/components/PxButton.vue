809b0a845b80c75d391a2d91116ff01cf86209f0
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2023-02-21 19:26:38 +0000

    Update forum.js

--- src/components/PxButton.vue
+++ src/components/PxButton.vue
@@ -1,4 +1,4 @@
 <template>
   <button
     @click="buttonClick"
-    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-
+    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:bo
