f3d7e9a54ac779ccb7493ee81745162cc146352f
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2023-02-25 11:42:03 +0000

    fixed copyright year

--- src/components/PxButton.vue
+++ src/components/PxButton.vue
@@ -1,4 +1,6 @@
 <template>
   <button
     @click="buttonClick"
-    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:bo
+    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded"
+  >
+    <beat-loa
