b8aea1325ba65d154512dda303310ddae9d82247
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2023-02-26 11:43:08 +0000

    luxor

--- src/components/PxButton.vue
+++ src/components/PxButton.vue
@@ -3,4 +3,4 @@
     @click="buttonClick"
     class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded"
   >
-    <beat-loa
+    <beat-loader :loading="isLoading" :color="'#68d391'" :s
