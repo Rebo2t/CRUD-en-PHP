f15674754fc251d5a7f1feaf09c75205bec34ddd
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2023-03-11 11:43:58 +0000

    Improved code layout.

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -2,4 +2,5 @@
   <header class="shadow">
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
-        <div class="flex items-center flex-shrink-0 te
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <px-icon class="m
