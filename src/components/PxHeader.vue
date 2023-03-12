bb73c523f3a96a5035b48ef9afc31d8a20fab7e4
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2023-03-12 13:38:59 +0000

    add picture

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -3,4 +3,6 @@
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-          <px-icon class="m
+          <px-icon class="mr-2" />
+          <router-link
+     
