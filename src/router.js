a87d6f609c1fa92b21daedbe7750403c4dea6967
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2023-04-24 22:05:05 +0000

    redist

---
+++ src/router.js
@@ -0,0 +1,12 @@
+
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Home from '@/views/Home'
+import Error from '@/views/Error'
+import About from '@/views/About'
+import CoinDetail from '@/views/CoinDetail'
+
+Vue.use(Router)
+
+export default new Router({
