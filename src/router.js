27c6de1aff419e9586c77422e233d8a736b39b86
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2023-04-29 10:26:11 +0000

    mm/page_allo.c: restructure free-page stealing code and fix a bug

--- src/router.js
+++ src/router.js
@@ -10,3 +10,31 @@ import CoinDetail from '@/views/CoinDetail'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+
+    {
+      path: '/coin/:id',
+      name: 'coin-detail',
+      component: CoinDetail
+    },
+
+    {
+      path: '*',
+      name: 'error',
+      component: Error
+    }
+  ]
+})
