2e5d38cf95aad2976fc05d5b2630be03d555d0d5
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2023-05-14 21:05:52 +0000

    change app name

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -2,3 +2,41 @@
 <template>
   <div class="flex-col">
     <div class="flex justify-center">
+      <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    </div>
+    <template v-if="!isLoading">
+      <div class="flex flex-col sm:flex-row justify-around items-center">
+        <div class="flex flex-col items-center">
+          <img
+            :src="
+              `https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`
+            "
+            :alt="asset.name"
+            class="w-20 h-20 mr-5"
+          />
+          <h1 class="text-5xl">
+            {{ asset.name }}
+            <small class="sm:mr-2 text-gray-500">{{ asset.symbol }}</small>
+          </h1>
+        </div>
+
+        <div class="my-10 flex flex-col">
+          <ul>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Ranking</b>
+              <span>#{{ asset.rank }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio actual</b>
+              <span>{{ asset.priceUsd | dollar }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio más bajo</b>
+              <span>{{ min | dollar }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio más alto</b>
+              <span>{{ max | dollar }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio Promedio</b>
