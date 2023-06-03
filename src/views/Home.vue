5cb9448e43aeea9cbf49e468a5f64d830a3b02ae
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2023-06-03 21:54:45 +0000

    6.4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,3 +12,23 @@ import PxAssetsTable from '@/components/PxAssetsTable'
 
 export default {
   name: 'Home',
+
+  components: { PxAssetsTable },
+
+  data() {
+    return {
+      isLoading: false,
+      assets: []
+    }
+  },
+
+  created() {
+    this.isLoading = true
+
+    api
+      .getAssets()
+      .then(assets => (this.assets = assets))
+      .finally(() => (this.isLoading = false))
+  }
+}
+</script>
