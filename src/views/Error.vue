4b3e7b3bde505951569379531916206ca64e88b5
Rebo2t <106934669+Rebo2t@users.noreply.github.com>
2023-06-03 06:25:11 +0000

    added bundle

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,4 +1,4 @@
 <template>
   <div class="flex-col items-center">
     <h1 class="text-gray-700 text-6xl uppercase">error</h1>
-    <router-link cla
+    <router-link class="mt-5 text-xl text-green-600 hover:u
